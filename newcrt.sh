255b2475b3679c4bb392493c1560ace484fe6501
Tim <tim@omarov.net>
2022-12-20 21:37:25 +0300

    minor

--- newcrt.sh
+++ newcrt.sh
@@ -10,4 +10,5 @@ openssl genrsa -out ca.key 2048
 openssl req -new -x509 -days 1024 -key ca.key -subj "/C=CN/ST=GD/L=SZ/O=Monsters Inc., Inc./CN=Monsters Inc. Root CA" -out ca.crt
 openssl req -newkey rsa:2048 -nodes -keyout $1.key -subj "/C=CN/ST=GD/L=SZ/O=Monsters Inc., Inc./CN=$1" -out $1.csr
 openssl x509 -req -extfile <(printf "subjectAltName=DNS:$1,DNS:gitlab") -days 1024 -in $1.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out $1.crt
-
+cp $1.crt gitlab.crt
+cp $1.key gitlab.key
